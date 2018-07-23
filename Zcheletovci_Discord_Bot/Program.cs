855ed50bf9a9461e90970dcd1613bd3b12edd95a
Mobster112 <marsicovski1@gmail.com>
2018-07-23 12:44:31 +0200

    Added the Discord Client and successfully connected!

--- Zcheletovci_Discord_Bot/Program.cs
+++ Zcheletovci_Discord_Bot/Program.cs
@@ -1,14 +1,41 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+using Discord;
 
 namespace Zcheletovci_Discord_Bot
 {
     class Program
     {
+        DiscordSocketClient _client;
+        CommandHandler _handler;
+
+
         static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
         {
-            Console.WriteLine("Token - " + Config.bot.token);
-            Console.WriteLine("Command Prefix - " + Config.bot.cmdPrefix);
-            Console.ReadLine();
+            if (Config.bot.token == "" || Config.bot.token == null) return;
+
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+
+            _client.Log += Log;
+
+
+            await _client.LoginAsync(TokenType.Bot, Config.bot.token);
+            await _client.StartAsync();
+            _handler = new CommandHandler();
+            await _handler.InitializeAsync(_client);
+            await Task.Delay(-1);
+        }
+
+        private async Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.Message);
         }
     }
 }
