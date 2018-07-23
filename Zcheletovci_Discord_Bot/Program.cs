5557e94cc69e139f1ecd4568adf567e7b8f204a3
Mobster112 <marsicovski1@gmail.com>
2018-07-23 11:57:44 +0200

    Add project files.

---
+++ Zcheletovci_Discord_Bot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zcheletovci_Discord_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
